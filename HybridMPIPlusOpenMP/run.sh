95302c011058e4e53f047680091f06f1c7adbe1d
brobey <brobey@lanl.gov>
2019-09-29 21:26:06 -0600

    Fixes to hybrid run

--- HybridMPIPlusOpenMP/run.sh
+++ HybridMPIPlusOpenMP/run.sh
@@ -2,5 +2,5 @@
 
 set -v
 
-export OMP_NUM_THREADS=2
-mpirun -n 44 --bind-to cores --mca btl_base_warn_component_unused 0 ./CartExchange -x 12 -y 12 -i 20000 -j 20000 -h 2 -t -c
+export OMP_NUM_THREADS=22
+mpirun -n 4 --bind-to socket ./CartExchange -x 2 -y 2 -i 20000 -j 20000 -h 2 -t -c
